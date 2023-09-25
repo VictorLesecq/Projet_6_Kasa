1ebda343b923ee1d837338460d727c9b8f9d0388
Victor Lesecq <victor.lesecq@gmail.com>
2023-09-25 19:49:34 +0200

    delete scroll restoration

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+     BrowserRouter as Router,
+     Routes,
+     Route,
+     useLocation,
+} from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -9,11 +14,23 @@ import GlobalStyle from './utils/style/GlobalStyle'
 import APropos from './pages/APropos'
 import Error from './components/Error'
 
+function ScrollToTop() {
+     const { pathname } = useLocation()
+
+     // Retourner toujours à la position de défilement supérieure lors de la navigation
+     React.useEffect(() => {
+          window.scrollTo(0, 0)
+     }, [pathname])
+
+     return null
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
      <React.StrictMode>
           <Router>
                <GlobalStyle />
+               <ScrollToTop />
                <Header />
                <Routes>
                     <Route path="/" element={<Home />} />
